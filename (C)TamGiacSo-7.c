19418d216eaa13c0a623343b39a66e36d5a13724
ThanhNQ <103249850+ThanhAT177@users.noreply.github.com>
2022-04-16 22:11:55 +0700

    Create (C)TamGiacSo-7.c
    Input    |     Output
      5         |     1 
                 |     2 6
                 |     3 7 10
                 |     4 8 11 13
                 |     5 9 12 14 15

---
+++ (C)TamGiacSo-7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+	int i, j, n, k, m, l;
+	scanf("%d", &n);
+	for(i = 1; i <= n; i++) {
+		for(j = 1; j <= i; j++) {
+			l = 0;
+			for(m = 1; m < j; m++) {
+				l += m;
+			}
+			printf("%d ", i - l + (j - 1) * n);
+		}
+		printf("\n");
+	}
+	return 0;
+}
